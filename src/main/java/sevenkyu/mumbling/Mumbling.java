2c5475f9d97a05fd45090091f761e1b2bc240604
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-06-17 08:39:27 +0200

    Mumbling second test pass

--- src/main/java/sevenkyu/mumbling/Mumbling.java
+++ src/main/java/sevenkyu/mumbling/Mumbling.java
@@ -1,8 +1,15 @@
 package sevenkyu.mumbling;
 
+import java.util.stream.Collectors;
+
 class Mumbling {
+    private static final String separator = "-";
 
     public String accum(String s) {
-        return s.toUpperCase();
+        return s.chars()
+            .boxed()
+            .map(Character::toUpperCase)
+            .map(Character::toString)
+            .collect(Collectors.joining(separator));
     }
 }
