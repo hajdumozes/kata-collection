359af9140127af69a95b561ffb06d828b6715512
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-06-23 08:21:44 +0200

    MiddleCharacter second case pass

--- src/main/java/sevenkyu/middlecharacter/MiddleCharacter.java
+++ src/main/java/sevenkyu/middlecharacter/MiddleCharacter.java
@@ -3,6 +3,7 @@ package sevenkyu.middlecharacter;
 class MiddleCharacter {
 
     public String getMiddle(String word) {
-        return word;
+        int middlePosition = (word.length() + 1) / 2;
+        return Character.toString(word.charAt(middlePosition - 1));
     }
 }
