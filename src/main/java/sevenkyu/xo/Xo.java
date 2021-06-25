1dfe19898c2fb1cecaa0a18e5470592d849f1138
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-06-25 08:29:11 +0200

    Xo second case pass

--- src/main/java/sevenkyu/xo/Xo.java
+++ src/main/java/sevenkyu/xo/Xo.java
@@ -3,6 +3,16 @@ package sevenkyu.xo;
 class Xo {
 
     public boolean getXO(String str) {
-        return true;
+        int xCounter = 0;
+        int oCounter = 0;
+        for (Character character : str.toCharArray()) {
+            if (character == 'x') {
+                xCounter++;
+            }
+            if (character == 'o') {
+                oCounter++;
+            }
+        }
+        return xCounter == oCounter;
     }
 }
