29e5f0f199b25a5be8148d9ee267dd645668ed04
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-07-13 08:24:12 +0200

    CreditCardMask case 1 pass

--- src/main/java/sevenkyu/creditcardmask/CreditCardMask.java
+++ src/main/java/sevenkyu/creditcardmask/CreditCardMask.java
@@ -1,8 +1,15 @@
 package sevenkyu.creditcardmask;
 
 public class CreditCardMask {
+    static final String MASKING_CHARACTER = "#";
 
     public String maskify(String str) {
-        return str;
+        if (str.length() <= 4) {
+            return str;
+        }
+        String charsToMask = str.substring(0, str.length() - 5);
+        String charsToKeep = str.substring(str.length() - 5);
+        String mask = charsToMask.replaceAll(".", MASKING_CHARACTER);
+        return mask + charsToKeep;
     }
 }
