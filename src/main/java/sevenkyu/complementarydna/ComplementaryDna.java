9840e686b7428e73159d849f22feef45147a6fec
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-07-14 08:04:01 +0200

    ComplementaryDna refactor into constant

--- src/main/java/sevenkyu/complementarydna/ComplementaryDna.java
+++ src/main/java/sevenkyu/complementarydna/ComplementaryDna.java
@@ -5,12 +5,12 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class ComplementaryDna {
-    Map<Character, Character> complements = Map.of('T', 'A', 'A', 'T', 'G', 'C', 'C', 'G');
+    static final Map<Character, Character> COMPLEMENTS = Map.of('T', 'A', 'A', 'T', 'G', 'C', 'C', 'G');
 
     public String makeComplement(String dna) {
         return IntStream.range(0, dna.length())
             .mapToObj(dna::charAt)
-            .map(character -> complements.get(character))
+            .map(COMPLEMENTS::get)
             .map(Object::toString)
             .collect(Collectors.joining());
     }
