c12c069b10c76d46207f735958e2e3622d3b5794
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-07-15 08:11:49 +0200

    TwoToOne refactor

--- src/main/java/sevenkyu/twotoone/TwoToOne.java
+++ src/main/java/sevenkyu/twotoone/TwoToOne.java
@@ -1,15 +1,12 @@
 package sevenkyu.twotoone;
 
-import java.util.stream.Collectors;
-
-public class TwoToOne {
+class TwoToOne {
 
     public String longest(String s1, String s2) {
         return s1.concat(s2).chars()
-            .mapToObj(character -> (char) character)
             .sorted()
             .distinct()
-            .map(String::valueOf)
-            .collect(Collectors.joining());
+            .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+            .toString();
     }
 }
