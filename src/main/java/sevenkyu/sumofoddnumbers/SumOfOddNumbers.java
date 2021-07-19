8d0d52943db20c4ad6e7c18ebb917564a1fc445c
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-07-19 08:19:20 +0200

    SumOfOddNumbers refactor

--- src/main/java/sevenkyu/sumofoddnumbers/SumOfOddNumbers.java
+++ src/main/java/sevenkyu/sumofoddnumbers/SumOfOddNumbers.java
@@ -4,21 +4,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 class SumOfOddNumbers {
+    static final int FIRST_ODD_NUMBER = 1;
+    static final Map<Integer, Integer> ROWS = new HashMap<>();
+
+    static {
+        ROWS.put(1, 1);
+    }
 
     public int rowSumOddNumbers(int n) {
-        int firstOddNumber = 1;
-        Map<Integer, Integer> rows = new HashMap<>();
-        rows.put(1, firstOddNumber);
-        int lastNumber = firstOddNumber;
+        int lastNumber = FIRST_ODD_NUMBER;
         int sum;
-        for (int i = 2; i <= n; i++) {
+        for (int rowNumber = 2; rowNumber <= n; rowNumber++) {
             sum = 0;
-            for (int k = 0; k < i; k++) {
+            for (int rowIterator = 0; rowIterator < rowNumber; rowIterator++) {
                 lastNumber += 2;
                 sum += lastNumber;
             }
-            rows.put(i, sum);
+            ROWS.put(rowNumber, sum);
         }
-        return rows.get(n);
+        return ROWS.get(n);
     }
 }
