4e6ed76bebedc91a1b327c35f73ecbea51e149be
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-07-19 08:30:42 +0200

    ValidatePin scer 2 case 2 green

--- src/main/java/sevenkyu/validatepin/ValidatePin.java
+++ src/main/java/sevenkyu/validatepin/ValidatePin.java
@@ -3,6 +3,9 @@ package sevenkyu.validatepin;
 class ValidatePin {
 
     public boolean validatePin(String pin) {
+        if (pin.length() == 4) {
+            return true;
+        }
         return false;
     }
 }
