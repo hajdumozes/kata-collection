f9075680acfe8b54b7220b17bcc0cbf335a9ff67
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-07-19 08:44:28 +0200

    ValidatePin scer 1 case 2 refactor

--- src/main/java/sevenkyu/validatepin/ValidatePin.java
+++ src/main/java/sevenkyu/validatepin/ValidatePin.java
@@ -1,11 +1,9 @@
 package sevenkyu.validatepin;
 
-import java.util.regex.Pattern;
-
 class ValidatePin {
-    static final Pattern PATTERN = Pattern.compile("\\d{4}|\\d{6}");
+    static final String PATTERN = "\\d{4}|\\d{6}";
 
     public boolean validatePin(String pin) {
-        return PATTERN.matcher(pin).matches();
+        return pin.matches(PATTERN);
     }
 }
