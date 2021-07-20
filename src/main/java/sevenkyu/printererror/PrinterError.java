18f02ec377c9316414e829c4dccab138561fa67b
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-07-20 07:14:15 +0200

    PrinterError scer 1 case 1 red

--- src/main/java/sevenkyu/printererror/PrinterError.java
+++ src/main/java/sevenkyu/printererror/PrinterError.java
@@ -2,4 +2,7 @@ package sevenkyu.printererror;
 
 class PrinterError {
 
+    public String printerError(String s) {
+        return null;
+    }
 }
--- src/test/java/sevenkyu/printererror/PrinterErrorTest.java
+++ src/test/java/sevenkyu/printererror/PrinterErrorTest.java
@@ -1,6 +1,9 @@
 package sevenkyu.printererror;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class PrinterErrorTest {
     PrinterError printerError;
@@ -8,5 +11,18 @@ class PrinterErrorTest {
     @BeforeEach
     void init() {
         printerError = new PrinterError();
+    }
+
+    @Test
+    void givenEmptyString_printerError_shouldPrintZeroOutOfZero() {
+        // given
+        String input = "";
+        String expected = "0/0";
+
+        // when
+        String output = printerError.printerError(input);
+
+        // then
+        assertThat(output).isEqualTo(expected);
     }
 }
