e12bd70831f6e43b646deca70b742a2b702aabb4
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-07-20 07:29:49 +0200

    PrinterError scer 2 case 2 green

--- src/main/java/sevenkyu/printererror/PrinterError.java
+++ src/main/java/sevenkyu/printererror/PrinterError.java
@@ -2,11 +2,10 @@ package sevenkyu.printererror;
 
 class PrinterError {
     static final Character SEPARATOR = '/';
+    static final String VALID_PATTERN = "[a-m]*";
 
     public String printerError(String s) {
-        if (s.contains("x")) {
-            return "1" + SEPARATOR + s.length();
-        }
-        return "0" + SEPARATOR + s.length();
+        String invalidChars = s.replaceAll(VALID_PATTERN, "");
+        return "" + invalidChars.length() + SEPARATOR + s.length();
     }
 }
