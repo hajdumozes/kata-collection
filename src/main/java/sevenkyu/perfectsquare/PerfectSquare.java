0d2a83bd29c53a577a68592cad81c28d4107d757
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-07-21 07:34:02 +0200

    PerfectSquare refactor

--- src/main/java/sevenkyu/perfectsquare/PerfectSquare.java
+++ src/main/java/sevenkyu/perfectsquare/PerfectSquare.java
@@ -1,11 +1,12 @@
 package sevenkyu.perfectsquare;
 
 class PerfectSquare {
+    static final long NON_SQUARE_RETURN_VALUE = -1;
 
     public long findNextSquare(long sq) {
         double sqrt = Math.sqrt(sq);
         if (sqrt != Math.floor(sqrt)) {
-            return -1;
+            return NON_SQUARE_RETURN_VALUE;
         }
         return (long) Math.pow(sqrt + 1, 2);
     }
