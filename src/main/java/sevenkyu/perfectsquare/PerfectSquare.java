e5d1eed92871220061241caa607777f2a3a855d3
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-07-21 07:31:50 +0200

    PerfectSquare scer 2 case 2 green

--- src/main/java/sevenkyu/perfectsquare/PerfectSquare.java
+++ src/main/java/sevenkyu/perfectsquare/PerfectSquare.java
@@ -3,9 +3,10 @@ package sevenkyu.perfectsquare;
 class PerfectSquare {
 
     public long findNextSquare(long sq) {
-        if (sq == 1) {
-            return 4;
+        double sqrt = Math.sqrt(sq);
+        if (sqrt != Math.floor(sqrt)) {
+            return -1;
         }
-        return -1;
+        return (long) Math.pow(sqrt + 1, 2);
     }
 }
--- src/main/java/sevenkyu/perfectsquare/notes.md
+++ src/main/java/sevenkyu/perfectsquare/notes.md
@@ -5,5 +5,5 @@ scenario 1: return -1 for not squares
 scenario 2: for squares, return next square number
 
     ✅ case 1: for input 1, return 4
-    ❗ case 2: for input 121, return 144
+    ✅ case 2: for input 121, return 144
     ❗ case 3: for input 625, return 676
