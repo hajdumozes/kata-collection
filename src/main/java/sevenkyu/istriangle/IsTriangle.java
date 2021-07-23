5db6401f1e94a46616b01ff78b3507eee4388666
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-07-23 08:30:15 +0200

    IsTriangle init

---
+++ src/main/java/sevenkyu/istriangle/IsTriangle.java
@@ -0,0 +1,8 @@
+package sevenkyu.istriangle;
+
+class IsTriangle {
+
+    public boolean isTriangle(int a, int b, int c) {
+        return false;
+    }
+}
---
+++ src/test/java/sevenkyu/istriangle/IsTriangleTest.java
@@ -0,0 +1,12 @@
+package sevenkyu.istriangle;
+
+import org.junit.jupiter.api.BeforeEach;
+
+class IsTriangleTest {
+    IsTriangle isTriangle;
+
+    @BeforeEach
+    void init() {
+        isTriangle = new IsTriangle();
+    }
+}
