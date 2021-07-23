79fa4303a891aa2e7843c430e3f91bf257355018
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-07-23 08:34:45 +0200

    IsTriangle case 1 test 1 red

---
+++ src/test/java/sevenkyu/triangle/TriangleTest.java
@@ -0,0 +1,30 @@
+package sevenkyu.triangle;
+
+import org.assertj.core.api.ThrowableAssert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class TriangleTest {
+    Triangle triangle;
+
+    @BeforeEach
+    void init() {
+        triangle = new Triangle();
+    }
+
+    @Test
+    void givenSideAIsSmallerThan1_isTriangle_shouldThrowError() {
+        // given
+        int a = 0;
+        int b = 1;
+        int c = 1;
+
+        // when
+        ThrowableAssert.ThrowingCallable isTriangle = () -> triangle.isTriangle(a, b, c);
+
+        // then
+        assertThatThrownBy(isTriangle).isInstanceOf(IllegalArgumentException.class);
+    }
+}
