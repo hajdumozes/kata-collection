ab859037fceb8b594a8eadd5b68b4602c4e69781
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-07-23 08:34:50 +0200

    IsTriangle case 1 test 1 green

--- src/main/java/sevenkyu/istriangle/IsTriangle.java
+++
@@ -1,8 +0,0 @@
-package sevenkyu.istriangle;
-
-class IsTriangle {
-
-    public boolean isTriangle(int a, int b, int c) {
-        return false;
-    }
-}
---
+++ src/main/java/sevenkyu/triangle/Triangle.java
@@ -0,0 +1,8 @@
+package sevenkyu.triangle;
+
+class Triangle {
+
+    public boolean isTriangle(int a, int b, int c) {
+        throw new IllegalArgumentException();
+    }
+}
