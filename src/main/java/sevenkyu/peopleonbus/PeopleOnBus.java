c6391cdef1a6827fecfafc981ad014408b3181a0
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-07-26 08:04:53 +0200

    PeopleOnBus refactor

--- src/main/java/sevenkyu/peopleonbus/PeopleOnBus.java
+++ src/main/java/sevenkyu/peopleonbus/PeopleOnBus.java
@@ -5,12 +5,8 @@ import java.util.List;
 class PeopleOnBus {
 
     public int countPassengers(List<int[]> stops) {
-        int in = 0;
-        int out = 0;
-        for (int[] stop : stops) {
-            in += stop[0];
-            out += stop[1];
-        }
-        return in - out;
+        return stops.stream()
+            .mapToInt(change -> change[0] - change[1])
+            .sum();
     }
 }
