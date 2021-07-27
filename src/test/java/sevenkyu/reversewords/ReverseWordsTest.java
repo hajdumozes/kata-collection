960db3a02513dbfea70506df1132d84470346c2e
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-07-27 06:56:35 +0200

    ReverseWords case 1 red

--- src/test/java/sevenkyu/reversewords/ReverseWordsTest.java
+++ src/test/java/sevenkyu/reversewords/ReverseWordsTest.java
@@ -1,6 +1,9 @@
 package sevenkyu.reversewords;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class ReverseWordsTest {
     ReverseWords reverseWords;
@@ -8,5 +11,18 @@ class ReverseWordsTest {
     @BeforeEach
     void init() {
         reverseWords = new ReverseWords();
+    }
+
+    @Test
+    void givenCharacter_reverse_shouldReturnItself() {
+        // given
+        String input = "o";
+        String expected = "o";
+
+        // when
+        String output = reverseWords.reverse(input);
+
+        // then
+        assertThat(output).isEqualTo(expected);
     }
 }
