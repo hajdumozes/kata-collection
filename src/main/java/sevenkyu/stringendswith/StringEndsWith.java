d30d7252e0f663c57c6c251cb8b0713564f6fb69
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-07-29 08:55:40 +0200

    StringEndsWith init

---
+++ src/main/java/sevenkyu/stringendswith/StringEndsWith.java
@@ -0,0 +1,8 @@
+package sevenkyu.stringendswith;
+
+class StringEndsWith {
+
+    public boolean solution(String str, String ending) {
+        return false;
+    }
+}
---
+++ src/test/java/sevenkyu/stringendswith/StringEndsWithTest.java
@@ -0,0 +1,13 @@
+package sevenkyu.stringendswith;
+
+import org.junit.jupiter.api.BeforeEach;
+
+class StringEndsWithTest {
+    StringEndsWith stringEndsWith;
+
+    @BeforeEach
+    void init() {
+        stringEndsWith = new StringEndsWith();
+    }
+
+}
