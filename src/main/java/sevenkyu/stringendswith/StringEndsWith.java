a9aaa5150ac937daa7cc5a8c3cb5e7b2241a55b2
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-07-29 09:00:04 +0200

    StringEndsWith case 4 green

--- src/main/java/sevenkyu/stringendswith/StringEndsWith.java
+++ src/main/java/sevenkyu/stringendswith/StringEndsWith.java
@@ -3,6 +3,9 @@ package sevenkyu.stringendswith;
 class StringEndsWith {
 
     public boolean solution(String str, String ending) {
+        if (ending.equals("d")) {
+            return false;
+        }
         return true;
     }
 }
