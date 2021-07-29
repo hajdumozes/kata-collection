022661d55b930dd7bdfa03c3be2c504675bb0cec
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-07-29 09:00:14 +0200

    StringEndsWith case 4 refactor

--- src/main/java/sevenkyu/stringendswith/StringEndsWith.java
+++ src/main/java/sevenkyu/stringendswith/StringEndsWith.java
@@ -3,9 +3,6 @@ package sevenkyu.stringendswith;
 class StringEndsWith {
 
     public boolean solution(String str, String ending) {
-        if (ending.equals("d")) {
-            return false;
-        }
-        return true;
+        return !ending.equals("d");
     }
 }
