ea269a17d82626868e9967c52ea1e9b1235ac73b
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-08-02 08:28:38 +0200

    Money case 1 red

--- src/test/java/sevenkyu/money/MoneyTest.java
+++ src/test/java/sevenkyu/money/MoneyTest.java
@@ -1,6 +1,9 @@
 package sevenkyu.money;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class MoneyTest {
     Money money;
@@ -8,5 +11,21 @@ class MoneyTest {
     @BeforeEach
     void init() {
         money = new Money();
+    }
+
+    @Test
+    void givenPrincipalIsSmallerThanDesired_calculateYears_shouldReturnWithZero() {
+        // given
+        int principal = 1000;
+        double interest = 0.10;
+        double tax = 0;
+        int desired = 1000;
+        int expectedYears = 0;
+
+        // when
+        int output = money.calculateYears(principal, interest, tax, desired);
+
+        // then
+        assertThat(output).isEqualTo(expectedYears);
     }
 }
