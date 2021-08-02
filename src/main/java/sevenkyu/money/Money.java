67c5460aa9d6facc3ed3edc731ec799b2b5f713d
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-08-02 08:36:27 +0200

    Money case 3 refactor

--- src/main/java/sevenkyu/money/Money.java
+++ src/main/java/sevenkyu/money/Money.java
@@ -3,9 +3,6 @@ package sevenkyu.money;
 class Money {
 
     public int calculateYears(double principal, double interest, double tax, double desired) {
-        if (principal >= desired) {
-            return 0;
-        }
         int year = 0;
         double currentMoney = principal;
         while (currentMoney < desired) {
