96ba2f9a11491dd05e8de26672cb1a58b78f13ae
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-08-03 08:34:51 +0200

    Fighting case 1 red

--- src/main/java/sevenkyu/fighting/notes.md
+++ src/main/java/sevenkyu/fighting/notes.md
@@ -1,10 +1,12 @@
 scenario: 1 turn fight
 
-- case 1: 1 hp & 1 (1/1) dmg for both -> starter wins
-- case 2: 4/5 for both -> starter wins
-- case 3: 2/1 & 2/2 -> bigger damage wins
+- case 1: 1/1 starter & 1/1 -> starter wins
+- case 2: 1/1 & 1/1 starter -> starter wins
+- case 3: 1 hp & 1 (1/1) dmg for both -> starter wins
+- case 4: 4/5 for both -> starter wins
+- case 5: 2/1 & 2/2 -> bigger damage wins
 
 scenario: multiple turn fight
 
-- case 1: 10/3 & 8/4 starter, -> 8/4 wins
-- case 1: 10/3 starter & 8/4, -> 10/3 wins
+- case 6: 10/3 & 8/4 starter, -> 8/4 wins
+- case 7: 10/3 starter & 8/4, -> 10/3 wins
--- src/test/java/sevenkyu/fighting/FightingTest.java
+++ src/test/java/sevenkyu/fighting/FightingTest.java
@@ -1,6 +1,9 @@
 package sevenkyu.fighting;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class FightingTest {
     Fighting fighting;
@@ -8,6 +11,20 @@ class FightingTest {
     @BeforeEach
     void init() {
         fighting = new Fighting();
+    }
+
+    @Test
+    void givenMinimumStatsFirstStarts_declareWinner_shouldReturnFirst() {
+        // given
+        String starter = "Scorpion";
+        Fighting.Fighter scorpion = new Fighting.Fighter(starter, 1, 1);
+        Fighting.Fighter subZero = new Fighting.Fighter("Sub Zero", 1, 1);
+
+        // when
+        String output = fighting.declareWinner(scorpion, subZero, starter);
+
+        // then
+        assertThat(output).isEqualTo(starter);
     }
 
 }
