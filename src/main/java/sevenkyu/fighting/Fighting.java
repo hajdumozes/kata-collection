844e1d7407869a06f17bb5ae8386704a71645149
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-08-03 08:57:11 +0200

    Fighting case 5 green

--- src/main/java/sevenkyu/fighting/Fighting.java
+++ src/main/java/sevenkyu/fighting/Fighting.java
@@ -1,15 +1,31 @@
 package sevenkyu.fighting;
 
+import java.util.List;
+
 class Fighting {
 
     public String declareWinner(Fighter fighter1, Fighter fighter2, String firstAttacker) {
-        if (fighter1.damagePerAttack > fighter2.damagePerAttack) {
-            return fighter1.name;
-        } else if (fighter1.damagePerAttack == fighter2.damagePerAttack) {
-            return firstAttacker;
-        } else {
-            return fighter2.name;
+        Fighter attacker = selectAttacker(fighter1, fighter2, firstAttacker);
+        Fighter defender = getOtherCharacter(attacker, fighter1, fighter2);
+        while (true) {
+            defender.health -= attacker.damagePerAttack;
+            if (defender.health <= 0) {
+                return attacker.name;
+            }
+            attacker = getOtherCharacter(attacker, fighter1, fighter2);
+            defender = getOtherCharacter(defender, fighter1, fighter2);
         }
+    }
+
+    private Fighter selectAttacker(Fighter fighter1, Fighter fighter2, String firstAttacker) {
+        return List.of(fighter1, fighter2).stream()
+            .filter(fighter -> fighter.name.equals(firstAttacker))
+            .findFirst()
+            .get();
+    }
+
+    private Fighter getOtherCharacter(Fighter current, Fighter fighter1, Fighter fighter2) {
+        return current == fighter1 ? fighter2 : fighter1;
     }
 
     public static class Fighter {
