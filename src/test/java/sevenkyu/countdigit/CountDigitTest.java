be609268f3b4fc9299ec4e02a4de7942919349f0
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-08-04 08:27:36 +0200

    CountDigit case 1 red

--- src/test/java/sevenkyu/countdigit/CountDigitTest.java
+++ src/test/java/sevenkyu/countdigit/CountDigitTest.java
@@ -1,6 +1,9 @@
 package sevenkyu.countdigit;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class CountDigitTest {
     CountDigit countDigit;
@@ -8,5 +11,19 @@ class CountDigitTest {
     @BeforeEach
     void init() {
         countDigit = new CountDigit();
+    }
+
+    @Test
+    void givenSquareTopIs0DigitIs0_nbDig_shouldReturn1() {
+        // given
+        int squareTop = 0;
+        int digit = 0;
+        int expected = 1;
+
+        // when
+        int output = countDigit.nbDig(squareTop, digit);
+
+        // then
+        assertThat(output).isEqualTo(expected);
     }
 }
