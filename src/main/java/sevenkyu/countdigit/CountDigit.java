cec8c47a14701868399739f929d6d8cdb98a60d3
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-08-04 08:29:17 +0200

    CountDigit case 2 green

--- src/main/java/sevenkyu/countdigit/CountDigit.java
+++ src/main/java/sevenkyu/countdigit/CountDigit.java
@@ -3,6 +3,9 @@ package sevenkyu.countdigit;
 class CountDigit {
 
     public int nbDig(int squareTop, int digit) {
+        if (digit == 1) {
+            return 0;
+        }
         return 1;
     }
 }
