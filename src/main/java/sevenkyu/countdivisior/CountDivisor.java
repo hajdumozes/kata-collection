bcac1c3e2ee291b4f3666302a6fafeb067b2f3d8
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-08-05 08:13:54 +0200

    CountDivisor case 2 green

--- src/main/java/sevenkyu/countdivisior/CountDivisor.java
+++ src/main/java/sevenkyu/countdivisior/CountDivisor.java
@@ -3,6 +3,9 @@ package sevenkyu.countdivisior;
 class CountDivisor {
 
     public long numberOfDivisors(int n) {
+        if (n == 2) {
+            return 2;
+        }
         return 1;
     }
 }
