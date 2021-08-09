f0904c916c3ae390b32f7bf11d8af95d2a4cff3e
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-08-09 07:23:55 +0200

    MinMaxProfit case 1 red

--- src/test/java/sevenkyu/minmaxprofit/MinMaxProfitTest.java
+++ src/test/java/sevenkyu/minmaxprofit/MinMaxProfitTest.java
@@ -1,6 +1,9 @@
 package sevenkyu.minmaxprofit;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class MinMaxProfitTest {
     MinMaxProfit minMaxProfit;
@@ -8,5 +11,18 @@ class MinMaxProfitTest {
     @BeforeEach
     void init() {
         minMaxProfit = new MinMaxProfit();
+    }
+
+    @Test
+    void givenOneLengthArray_minMax_shouldReturnNumberTwice() {
+        // given
+        int[] input = new int[]{1};
+        int[] expected = new int[]{1, 1};
+
+        // when
+        int[] output = minMaxProfit.minMax(input);
+
+        // then
+        assertThat(output).isEqualTo(expected);
     }
 }
