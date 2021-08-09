e25e4d1ea2a25291034da6ca9df6cf455fe3c90b
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-08-09 07:25:36 +0200

    MinMaxProfit case 2 green

--- src/main/java/sevenkyu/minmaxprofit/MinMaxProfit.java
+++ src/main/java/sevenkyu/minmaxprofit/MinMaxProfit.java
@@ -3,6 +3,6 @@ package sevenkyu.minmaxprofit;
 class MinMaxProfit {
 
     public int[] minMax(int[] arr) {
-        return new int[]{arr[0], arr[0]};
+        return new int[]{arr[0], arr[arr.length - 1]};
     }
 }
--- src/main/java/sevenkyu/minmaxprofit/notes.md
+++ src/main/java/sevenkyu/minmaxprofit/notes.md
@@ -4,6 +4,6 @@ scenario: 1 length array
 
 scenario 2: multiple numbers
 
-- case 2: 1, 2, 3, 4, 5 -> 1, 5
+- ✅ case 2: 1, 2, 3, 4, 5 -> 1, 5
 
 - case 3: 19, 2, 5 -> 2, 19
