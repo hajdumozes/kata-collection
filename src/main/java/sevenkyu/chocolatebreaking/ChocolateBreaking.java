b15e168eb58dbe4614d8c73cfe8b6add158bbc91
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-08-11 07:48:59 +0200

    ChocolateBreaking case 7 refactor

--- src/main/java/sevenkyu/chocolatebreaking/ChocolateBreaking.java
+++ src/main/java/sevenkyu/chocolatebreaking/ChocolateBreaking.java
@@ -2,9 +2,6 @@ package sevenkyu.chocolatebreaking;
 
 class ChocolateBreaking {
     public int breakChocolate(int n, int m) {
-        if (n == 0 || m == 0) {
-            return 0;
-        }
-        return n * m - 1;
+        return Math.max(n * m - 1, 0);
     }
 }
