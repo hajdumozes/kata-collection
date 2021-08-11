1e007498122acd18c708165d1fac7a729fba2005
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-08-11 07:41:27 +0200

    ChocolateBreaking case 3 green

--- src/main/java/sevenkyu/chocolatebreaking/ChocolateBreaking.java
+++ src/main/java/sevenkyu/chocolatebreaking/ChocolateBreaking.java
@@ -2,9 +2,6 @@ package sevenkyu.chocolatebreaking;
 
 class ChocolateBreaking {
     public int breakChocolate(int n, int m) {
-        if (m == 2) {
-            return 1;
-        }
-        return 0;
+        return m - 1;
     }
 }
