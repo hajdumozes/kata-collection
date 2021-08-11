62e8a3cf45bab47ec523f2e5a8dbe4ccefcc0b7c
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-08-11 07:46:25 +0200

    ChocolateBreaking case 7 green

--- src/main/java/sevenkyu/chocolatebreaking/ChocolateBreaking.java
+++ src/main/java/sevenkyu/chocolatebreaking/ChocolateBreaking.java
@@ -2,7 +2,7 @@ package sevenkyu.chocolatebreaking;
 
 class ChocolateBreaking {
     public int breakChocolate(int n, int m) {
-        if (n == 0) {
+        if (n == 0 || m == 0) {
             return 0;
         }
         return n * m - 1;
--- src/main/java/sevenkyu/chocolatebreaking/notes.md
+++ src/main/java/sevenkyu/chocolatebreaking/notes.md
@@ -15,4 +15,4 @@ scenario: multiple rows (multiplication - 1 needed)
 scenario: invalid input (return 0)
 
 - ✅ case 6: 0x5 -> 0
-- case 7: 5x0 -> 0
+- ✅ case 7: 5x0 -> 0
