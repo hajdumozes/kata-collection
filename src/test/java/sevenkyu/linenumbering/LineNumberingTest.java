1689f5f4468523691eda43a1efae0f2cee59c1d4
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-08-12 07:41:31 +0200

    LineNumbering case 1 red

--- src/test/java/sevenkyu/linenumbering/LineNumberingTest.java
+++ src/test/java/sevenkyu/linenumbering/LineNumberingTest.java
@@ -1,6 +1,11 @@
 package sevenkyu.linenumbering;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LineNumberingTest {
     LineNumbering lineNumbering;
@@ -8,5 +13,18 @@ class LineNumberingTest {
     @BeforeEach
     void init() {
         lineNumbering = new LineNumbering();
+    }
+
+    @Test
+    void givenOneChar_number_shouldReturnFirstLineAndChar() {
+        // given
+        List<String> input = List.of("a");
+        List<String> expected = List.of("1: a");
+
+        // when
+        List<String> output = lineNumbering.number(input);
+
+        // then
+        assertThat(output).isEqualTo(expected);
     }
 }
