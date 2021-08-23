058b9bbc60c2b3ee6964bc87efd22d1805f4fb4d
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-08-23 08:35:14 +0200

    MaxLengthDiff case 1 red

--- src/test/java/sevenkyu/maxlengthdiff/MaxLengthDiffTest.java
+++ src/test/java/sevenkyu/maxlengthdiff/MaxLengthDiffTest.java
@@ -1,6 +1,9 @@
 package sevenkyu.maxlengthdiff;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class MaxLengthDiffTest {
     MaxLengthDiff maxLengthDiff;
@@ -8,5 +11,19 @@ class MaxLengthDiffTest {
     @BeforeEach
     void init() {
         maxLengthDiff = new MaxLengthDiff();
+    }
+
+    @Test
+    void givenA1IsEmpty_mxdiflg_shouldReturnMinusOne() {
+        // given
+        String[] a1 = new String[]{};
+        String[] a2 = new String[]{"a"};
+        int expected = -1;
+
+        // when
+        int output = maxLengthDiff.mxdiflg(a1, a2);
+
+        // then
+        assertThat(output).isEqualTo(expected);
     }
 }
