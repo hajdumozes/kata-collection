2fb7c332466affecaca048e58996c1e06f8c874d
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-08-23 08:37:37 +0200

    MaxLengthDiff case 3 green

--- src/main/java/sevenkyu/maxlengthdiff/MaxLengthDiff.java
+++ src/main/java/sevenkyu/maxlengthdiff/MaxLengthDiff.java
@@ -3,6 +3,9 @@ package sevenkyu.maxlengthdiff;
 class MaxLengthDiff {
 
     public int mxdiflg(String[] a1, String[] a2) {
-        return -1;
+        if (a1.length == 0 || a2.length == 0) {
+            return -1;
+        }
+        return 0;
     }
 }
--- src/main/java/sevenkyu/maxlengthdiff/notes.md
+++ src/main/java/sevenkyu/maxlengthdiff/notes.md
@@ -5,7 +5,7 @@ scenario: either empty
 
 scenario: one element
 
-- case 3: both 1 char long -> return 0
+- ✅ case 3: both 1 char long -> return 0
 - case 4: s1 is 1 char longer -> return 1
 - case 5: s2 is 1 char longer -> return -1
 
