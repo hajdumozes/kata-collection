a7afbf7b80fcd2dd719ac9c7f1a3d118c51eed2a
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-08-30 08:37:08 +0200

    ReverseLetter case 1 red

--- src/test/java/sevenkyu/reverseletter/ReverseLetterTest.java
+++ src/test/java/sevenkyu/reverseletter/ReverseLetterTest.java
@@ -1,6 +1,9 @@
 package sevenkyu.reverseletter;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class ReverseLetterTest {
     ReverseLetter reverseLetter;
@@ -8,5 +11,18 @@ class ReverseLetterTest {
     @BeforeEach
     void init() {
         reverseLetter = new ReverseLetter();
+    }
+
+    @Test
+    void givenOneCharLongLetter_reverseLetter_shouldReturnLetter() {
+        // given
+        String input = "a";
+        String expected = "a";
+
+        // when
+        String output = reverseLetter.reverseLetter(input);
+
+        // then
+        assertThat(output).isEqualTo(expected);
     }
 }
